2f7bb6875638e2d999c1b4b2e434789e5cd97c4a
yosiaofficial <pocoyoomy@gmail.com>
2022-09-28 21:31:44 +0700

    <message>

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '/logo.svg';
+impot '/App.css';
+import gambar1 from './.vscode/assets/1.png';
+
+function App () {
+    <div className='App'>
+        <header className='App-header'>
+            <img src='gambar1' className='App-logo' alt='gambar1'/>
+            <p>
+                Edit <code>src.App.js</code> and save to reload.
+                </p>
+            <a
+            className='App-link'
+            href='https://reactjs.org'
+            target="_blank"
+            rel='noope'
+            ></a>     
+        </header>
+    </div>
+}
+ 
